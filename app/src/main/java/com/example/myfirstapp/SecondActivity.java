44330976598bbb29ea00de65a03ba5a7ad53df33
nmqsrna <masqasrina@gmail.com>
2022-11-07 17:50:48 +0800

    SecondCommit

--- app/src/main/java/com/example/myfirstapp/SecondActivity.java
+++ app/src/main/java/com/example/myfirstapp/SecondActivity.java
@@ -9,27 +9,46 @@ import android.widget.TextView;
 public class SecondActivity extends AppCompatActivity {
 
     public  final static String MESSAGE_KEY = "com.example.MyFirstApp.message_key";
+    public  final static String EMAIL = "com.example.MyFirstApp.email";
     public  final static String BIRTHDATE = "com.example.MyFirstApp.birthdate";
     public  final static String ADDRESS = "com.example.MyFirstApp.address";
     public  final static String PHONE_NUMBER = "com.example.MyFirstApp.phone_number";
 
+    TextView tv_UserName, tv_Email, tv_Birthday, tv_Address, tv_PhoneNum;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+
+        tv_UserName = findViewById(R.id.tvUserName);
+        tv_Email = findViewById(R.id.tvEmail);
+        tv_Birthday = findViewById(R.id.tvBirthday);
+        tv_Address = findViewById(R.id.tvAddress);
+        tv_PhoneNum = findViewById(R.id.tvPhoneNum);
 
         Intent intent = getIntent();
+
         String message = intent.getStringExtra(MESSAGE_KEY);
-        String message1 = intent.getStringExtra(BIRTHDATE);
-        String message2 = intent.getStringExtra(ADDRESS);
-        String message3 = intent.getStringExtra(PHONE_NUMBER);
+        String message1 = intent.getStringExtra(EMAIL);
+        String message2 = intent.getStringExtra(BIRTHDATE);
+        String message3 = intent.getStringExtra(ADDRESS);
+        String message4 = intent.getStringExtra(PHONE_NUMBER);
 
-        TextView textView = new TextView(this);
-        textView.setTextSize(25);
-        textView.setText("Name : " +message +"\nBirth Date : " +message1 +"\nAddress : "+message2 +"\nPhone Num. : "+message3);
-        setContentView(textView);
+        tv_UserName.setTextSize(24);
+        tv_UserName.setText("Name : " +message );
 
+        tv_Email.setTextSize(24);
+        tv_Email.setText("\nEmail : " +message1);
+
+        tv_Birthday.setTextSize(24);
+        tv_Birthday.setText("\nBirth Date : " +message2);
+
+        tv_Address.setTextSize(24);
+        tv_Address.setText("\nAddress : "+message3);
+
+        tv_PhoneNum.setTextSize(24);
+        tv_PhoneNum.setText("\nPhone Number: " +message4);
 
     }
-
-
 }
